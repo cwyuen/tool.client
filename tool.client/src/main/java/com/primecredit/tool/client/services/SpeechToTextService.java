bf6d79da5aa4e146877b877b52a30e07193db6c3
maxwellyuen <maxwellyuen@192.168.19.116>
2017-08-03 19:33:05 +0800

    Signed-off-by: maxwellyuen <maxwellyuen@192.168.19.116>

--- tool.client/src/main/java/com/primecredit/tool/client/services/SpeechToTextService.java
+++ tool.client/src/main/java/com/primecredit/tool/client/services/SpeechToTextService.java
@@ -1,6 +1,8 @@
 package com.primecredit.tool.client.services;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -29,9 +31,12 @@ public class SpeechToTextService {
 
 	@Autowired
 	private SystemConfig systemConfig;
-	
+
 	@Autowired
 	private SpeakerIdentificationService speakerIdentificationService;
+
+	@Autowired
+	private SpeechRecongnitionService speechRecongnitionService;
 
 	public void run() throws Exception {
 		// (0) init var
@@ -42,32 +47,70 @@ public class SpeechToTextService {
 
 		// (2) List all Wav file
 		List<String> wavFiles = wavFileHandler.listWavFiles(systemConfig.getWavPath());
-		
+
 		Iterator<String> wavIter = wavFiles.iterator();
-		while(wavIter.hasNext()) {
+		while (wavIter.hasNext()) {
 			String sourceFileName = wavIter.next();
-			logger.info("File Name: {}", sourceFileName);;
-			
+			logger.info("File Name: {}", sourceFileName);
+			;
+
 			File sourceFile = new File(sourceFileName);
 			String shortFileName = sourceFile.getName();
-			
-			//(3) Speaker Diarization
+
+			// (3) Speaker Diarization
 			List<DiarizationSpeech> dsList = speakerIdentificationService.diarization(sourceFileName);
-			
-			//(4) Split Wav file by speaker
-			if(dsList != null) {
+
+			// (4) Split Wav file by speaker
+			if (dsList != null) {
 				int count = 1;
-				for(DiarizationSpeech ds: dsList) {
+				for (DiarizationSpeech ds : dsList) {
 					int startLen = ds.getSegmentStart();
 					int segmentLen = ds.getSegmentLen();
 					String distFile = systemConfig.getTempPath() + String.valueOf(count) + "_" + shortFileName;
 					ds.setSourceFileName(distFile);
-					wavFileHandler.copyWavAudioBySecond(sourceFileName, distFile, (double) startLen, (double) segmentLen);
-					count++;		
-					
+					wavFileHandler.copyWavAudioBySecond(sourceFileName, distFile, (double) startLen,
+							(double) segmentLen);
+					count++;
+
 				}
 			}
+
+			// (5) Call Speech Convert Service
+			for (DiarizationSpeech ds : dsList) {
+				List<String> speechTextList = speechRecongnitionService.convert(ds.getSourceFileName());
+				ds.setSpeechTextList(speechTextList);
+			}
+			
+
+			//(99) Export to file Result
+			int index = sourceFileName.indexOf(".");
+			String filefirstName = sourceFileName.substring(0, index);
+			//String ext = sourceFileName.substring(index);
+			File textFile = new File(filefirstName + ".txt");
+			
+			try (FileWriter fw = new FileWriter(textFile); BufferedWriter bw = new BufferedWriter(fw)) {
+				
+				for(int version = 1; version<=5; version++) {
+					bw.write("Version (" + version + ")");
+					bw.write("\n");
+					for(DiarizationSpeech ds: dsList) {
+						
+						bw.write(ds.getName() + " : " + ds.getSpeechTextList().get(version-1));
+						bw.write("\n");
+
+					}
+					
+					bw.write("==============================================");
+					bw.write("\n");
+					bw.write("\n");
+				}
+				bw.close();
+				fw.close();
+			}
+			
+			
 		}
+		
 	}
 
 	public void test() {
